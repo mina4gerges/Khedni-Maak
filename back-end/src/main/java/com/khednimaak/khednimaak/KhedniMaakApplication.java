f5b0fa9f0994dcb4fd48961dbe05c3c1182a1e7c
mina gerges <minagerges4@gmail.com>
2020-08-15 10:39:20 +0300

    set back end structure

---
+++ back-end/src/main/java/com/khednimaak/khednimaak/KhedniMaakApplication.java
@@ -0,0 +1,13 @@
+package com.khednimaak.khednimaak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KhedniMaakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KhedniMaakApplication.class, args);
+    }
+
+}
---
+++ back-end/src/test/java/com/khednimaak/khednimaak/KhedniMaakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khednimaak.khednimaak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KhedniMaakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
