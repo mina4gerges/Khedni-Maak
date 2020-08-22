e9db4e2c0dab388190f40bc2d1139414b48bf099
mina gerges <minagerges4@gmail.com>
2020-08-22 18:27:23 +0300

    new introduction screen

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-
-import 'login/login_screen.dart';
-import 'login/dashboard_screen.dart';
-import 'login/transition_route_observer.dart';
+import 'package:khedni_maak/introductionScreen/introduction_screen.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -19,44 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Page',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        accentColor: Colors.orange,
-        cursorColor: Colors.orange,
-        textTheme: TextTheme(
-          headline3: TextStyle(
-            fontFamily: 'OpenSans',
-            fontSize: 45.0,
-            color: Colors.orange,
-          ),
-          button: TextStyle(
-            fontFamily: 'OpenSans',
-          ),
-          caption: TextStyle(
-            fontFamily: 'NotoSans',
-            fontSize: 12.0,
-            fontWeight: FontWeight.normal,
-            color: Colors.deepPurple[300],
-          ),
-          headline1: TextStyle(fontFamily: 'Quicksand'),
-          headline2: TextStyle(fontFamily: 'Quicksand'),
-          headline4: TextStyle(fontFamily: 'Quicksand'),
-          headline5: TextStyle(fontFamily: 'NotoSans'),
-          headline6: TextStyle(fontFamily: 'NotoSans'),
-          subtitle1: TextStyle(fontFamily: 'NotoSans'),
-          bodyText1: TextStyle(fontFamily: 'NotoSans'),
-          bodyText2: TextStyle(fontFamily: 'NotoSans'),
-          subtitle2: TextStyle(fontFamily: 'NotoSans'),
-          overline: TextStyle(fontFamily: 'NotoSans'),
-        ),
-      ),
-      home: LoginScreen(),
-      navigatorObservers: [TransitionRouteObserver()],
-      routes: {
-        LoginScreen.routeName: (context) => LoginScreen(),
-        DashboardScreen.routeName: (context) => DashboardScreen(),
-      },
-    );
+        title: 'Start screen',
+        home: IntroductionView());
   }
 }
