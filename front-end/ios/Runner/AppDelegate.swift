e4b1fed1bac61e926c66024614c4883db75872b5
mina gerges <minagerges4@gmail.com>
2020-08-28 00:30:00 +0300

    add new config for IOS and android for maps (geoLocation)

--- front-end/ios/Runner/AppDelegate.swift
+++ front-end/ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyALdBwMTDH4Ly56r7mxCYilWe2PBz_zj0g")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
