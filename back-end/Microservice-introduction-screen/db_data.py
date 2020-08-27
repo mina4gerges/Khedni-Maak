e8b9072b260319a09f56c118317e127400bf46b3
mina gerges <minagerges4@gmail.com>
2020-08-28 01:23:59 +0300

    init new microservice using python (flask) and mySql for introductions screens

---
+++ back-end/Microservice-introduction-screen/db_data.py
@@ -0,0 +1,15 @@
+host = ""
+dbname = ""
+user = ""
+password = ""
+
+# TABLE creation:
+
+# create table screens
+# (
+#     intro_id    int auto_increment
+#         primary key,
+#     intro_title varchar(50) null,
+#     intro_desc  varchar(50) null,
+#     intro_image varchar(50) null
+# );
