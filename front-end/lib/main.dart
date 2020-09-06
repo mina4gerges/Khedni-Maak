3ca7c96a6764dddc5731c1be0cf9130e30d35029
mina gerges <minagerges4@gmail.com>
2020-09-06 13:30:55 +0300

    new bottom nav bar

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:khedni_maak/google_map/map_main.dart';
+import 'package:khedni_maak/testFiles/firebase/add_user.dart';
 //import 'package:khedni_maak/test_widgets/post.dart';
 
 import 'introduction_screen/introduction_screen.dart';
+import 'nav_bar/nav_bar_main.dart';
 
 void main() {
   SystemChrome.setSystemUIOverlayStyle(
@@ -18,7 +20,8 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'Start screen', home: MapMain()
+    return MaterialApp(title: 'Start screen', home: NavBarMain()
+//        home: MapMain()
 //        home: IntroductionView()
         //        home: PostsPage());
 
