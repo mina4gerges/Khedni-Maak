abe25a33411cb71970f40d448915c9ea8a6a4a58
mina.gerges <minagerges4@gmail.com>
2020-10-02 16:07:28 +0300

    working on places map

---
+++ front-end/lib/google_map/test_map/new/src/controllers/autocomplete_search_controller.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/cupertino.dart';
+
+import '../autocomplete_search.dart';
+
+class SearchBarController extends ChangeNotifier {
+  AutoCompleteSearchState _autoCompleteSearch;
+
+  attach(AutoCompleteSearchState searchWidget) {
+    _autoCompleteSearch = searchWidget;
+  }
+
+  /// Just clears text.
+  clear() {
+    _autoCompleteSearch.clearText();
+  }
+
+  /// Clear and remove focus (Dismiss keyboard)
+  reset() {
+    _autoCompleteSearch.resetSearchBar();
+  }
+
+  clearOverlay() {
+    _autoCompleteSearch.clearOverlay();
+  }
+}
---
+++ front-end/lib/google_map/test_map/secrets.dart
@@ -0,0 +1,4 @@
+class Secrets {
+  // Add your Google Maps API Key here
+  static const API_KEY = 'AIzaSyCRFrdnwMkAr-rhTvr9Z1CWIS3kYeo8o6Y';
+}
