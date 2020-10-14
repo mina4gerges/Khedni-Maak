722588fb36aa3c0c06ba6d693da0ede03c270574
mina gerges <minagerges4@gmail.com>
2020-10-15 02:38:49 +0300

    change materialApp title

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Start screen',
+      title: 'Khedni Maak',
       home: IntroductionView(),
       debugShowCheckedModeBanner: false,
       navigatorObservers: [TransitionRouteObserver()],
