60256bfedb7cb88c594f5111eb6cd7993dbede08
mina gerges <minagerges4@gmail.com>
2020-10-14 19:47:48 +0300

    add CustomScrollView to dashboard screen

--- front-end/lib/screens/dashbaord_screen.dart
+++ front-end/lib/screens/dashbaord_screen.dart
@@ -5,44 +5,61 @@ import 'package:khedni_maak/login/custom_route.dart';
 import 'package:khedni_maak/widgets/dashboard_header.dart';
 
 class DashboardScreen extends StatelessWidget {
+  DashboardScreen({Key key}) : super(key: key);
   static const routeName = '/auth/DashboardScreen';
 
   @override
   Widget build(BuildContext context) {
-
+    const Key centerKey = ValueKey('bottom-sliver-list');
     final screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      // appBar: CustomAppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: <Widget>[
-          DashboardHeader(screenHeight: screenHeight),
-          SizedBox(height: screenHeight * 0.03),
-          Column(
-            children: <Widget>[
-              BuildPicCard(
-                title: 'Offer a ride',
-                body: 'You can share your car with someone',
-                pic: 'driver.png',
-                screenHeight: screenHeight,
-                onCardTap: () {
-                  Navigator.of(context).push(
-                      FadePageRoute(builder: (_) => NavBarMain(source: 'driver')));
-                },
-              ),
-              BuildPicCard(
-                title: 'Find a ride',
-                body: 'You can find someone is sharing his car',
-                pic: 'truck.png',
-                screenHeight: screenHeight,
-                onCardTap: () {
-                  Navigator.of(context).push(
-                      FadePageRoute(builder: (_) => NavBarMain(source: 'rider')));
-                },
-              ),
-              SizedBox(height: screenHeight * 0.05),
-            ],
+      body: CustomScrollView(
+        center: centerKey,
+        slivers: <Widget>[
+          SliverList(
+            key: centerKey,
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      DashboardHeader(screenHeight: screenHeight),
+                      SizedBox(height: screenHeight * 0.03),
+                      Column(
+                        children: <Widget>[
+                          BuildPicCard(
+                            title: 'Offer a ride',
+                            body: 'You can share your car with someone',
+                            pic: 'driver.png',
+                            screenHeight: screenHeight,
+                            onCardTap: () {
+                              Navigator.of(context).push(FadePageRoute(
+                                  builder: (_) =>
+                                      NavBarMain(source: 'driver')));
+                            },
+                          ),
+                          BuildPicCard(
+                            title: 'Find a ride',
+                            body: 'You can find someone is sharing his car',
+                            pic: 'truck.png',
+                            screenHeight: screenHeight,
+                            onCardTap: () {
+                              Navigator.of(context).push(FadePageRoute(
+                                  builder: (_) => NavBarMain(source: 'rider')));
+                            },
+                          ),
+                          SizedBox(height: screenHeight * 0.05),
+                        ],
+                      ),
+                    ],
+                  ),
+                );
+              },
+              childCount: 1,
+            ),
           ),
         ],
       ),
