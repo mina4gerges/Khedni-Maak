0fb17db7e886bc2e527a3787aabeab73723e2105
mina gerges <minagerges4@gmail.com>
2020-10-31 17:09:28 +0200

    add new routes

--- front-end/lib/main.dart
+++ front-end/lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:khedni_maak/screens/map_screen.dart';
+import 'package:khedni_maak/screens/rides_screen.dart';
+import 'config/Secrets.dart';
 import 'login/login_screen.dart';
 import 'widgets/nav_bar_main.dart';
 import 'login/transition_route_observer.dart';
@@ -26,6 +30,15 @@ class MyApp extends StatelessWidget {
         },
         '/auth/DashboardScreen/navBar': (BuildContext context) {
           return NavBarMain(source: 'driver');
+        },
+        '/auth/DashboardScreen/navBar/mapScreen': (BuildContext context) {
+         return MapScreen(apiKey: Secrets.API_KEY, initialPosition: LatLng(0, 0));
+        },
+        '/auth/DashboardScreen/navBar/driverRidesScreen': (BuildContext context) {
+          return RidesScreen(source:'driver');
+        },
+        '/auth/DashboardScreen/navBar/riderRidesScreen': (BuildContext context) {
+          return RidesScreen(source:'rider');
         },
       },
     );
