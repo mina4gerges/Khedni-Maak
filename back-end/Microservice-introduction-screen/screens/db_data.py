9179e3e6a6739b7223f9c50e685c4b0ca5716070
mina gerges <minagerges4@gmail.com>
2020-11-02 20:08:28 +0200

    fix introducation screen API

---
+++ back-end/Microservice-introduction-screen/screens/db_data.py
@@ -0,0 +1,11 @@
+# TABLE creation:
+#
+# create table screens
+# (
+#     intro_id    int auto_increment
+#         primary key,
+#     intro_title varchar(50) null,
+#     intro_desc  varchar(50) null,
+#     intro_image varchar(50) null,
+#     intro_image_path varchar(50) null,
+# );
