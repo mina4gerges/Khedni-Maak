4526c04a3097c1d7caaa314a846163b7d252bcbc
mina gerges <minagerges4@gmail.com>
2020-11-04 23:11:19 +0200

    fix introduction screen to get data from fb

--- front-end/lib/screens/introduction_screen.dart
+++ front-end/lib/screens/introduction_screen.dart
@@ -1,24 +1,20 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:introduction_screen/introduction_screen.dart';
+import 'package:http/http.dart' as http;
 import 'package:khedni_maak/config/palette.dart';
+import 'package:khedni_maak/config/constant.dart';
 import 'package:khedni_maak/login/login_screen.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class IntroductionView extends StatelessWidget {
   static const routeName = '/';
 
-  @override
-  Widget build(BuildContext context) {
-    return OnBoardingPage();
-  }
-}
-
-class OnBoardingPage extends StatefulWidget {
-  @override
-  _OnBoardingPageState createState() => _OnBoardingPageState();
-}
-
-class _OnBoardingPageState extends State<OnBoardingPage> {
   final introKey = GlobalKey<IntroductionScreenState>();
+
+  Future _getIntroductionScreens() async {
+    return await http.get('$baseUrlIntroductionScreen/screens');
+  }
 
   void _onIntroEnd(context) {
     Navigator.of(context).push(
@@ -33,8 +29,7 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
+  _displayPage(Map screen) {
     const bodyStyle = TextStyle(fontSize: 19.0);
 
     const pageDecoration = const PageDecoration(
@@ -45,52 +40,69 @@ class _OnBoardingPageState extends State<OnBoardingPage> {
       titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
     );
 
-    return IntroductionScreen(
-      key: introKey,
-      pages: [
-        PageViewModel(
-          body: "Intro 1",
-          title: "Khedni Maak 1",
-          decoration: pageDecoration,
-          image: _buildImage('introduction1'),
-        ),
-        PageViewModel(
-          body: "Intro 2",
-          title: "Khedni Maak 2",
-          decoration: pageDecoration,
-          image: _buildImage('introduction2'),
-        ),
-        PageViewModel(
-          body: "Intro 3",
-          title: "Khedni Maak 3",
-          decoration: pageDecoration,
-          image: _buildImage('introduction3'),
-        ),
-        PageViewModel(
-          body: "Intro 4",
-          title: "Khedni Maak 4",
-          decoration: pageDecoration,
-          image: _buildImage('introduction4'),
-        ),
-      ],
-      skipFlex: 0,
-      nextFlex: 0,
-      showSkipButton: true,
-      curve: Curves.bounceIn,
-      skip: const Text('Skip'),
-      onDone: () => _onIntroEnd(context),
-      onSkip: () => _onIntroEnd(context),
-      next: const Icon(Icons.arrow_forward),
-      done: const Text('Done', style: TextStyle(fontWeight: FontWeight.w600)),
-      dotsDecorator: const DotsDecorator(
-        size: Size(10.0, 10.0),
-        color: Color(0xFFBDBDBD),
-        activeSize: Size(22.0, 10.0),
-        activeColor: Palette.primaryColor,
-        activeShape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(25.0)),
-        ),
-      ),
+    return PageViewModel(
+      body: screen["body"],
+      title: screen["title"],
+      decoration: pageDecoration,
+      image: _buildImage(screen["image"]),
+    );
+  }
+
+  _displayPages(List screens) {
+    List<PageViewModel> finalScreens = [];
+
+    for (int i = 0; i < screens.length; i++) {
+      finalScreens.add(_displayPage(screens[i]));
+    }
+
+    return finalScreens;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: _getIntroductionScreens(),
+      builder: (context, snap) {
+        if (snap.connectionState == ConnectionState.waiting)
+          return const Center(child: CircularProgressIndicator());
+        else if (snap.data == null) {
+          return Center(
+            child: Text(
+              "Error occurred",
+              style: TextStyle(
+                color: Colors.red,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          );
+        } else {
+          List screens = json.decode(snap.data.body);
+
+          return IntroductionScreen(
+            key: introKey,
+            pages: _displayPages(screens),
+            skipFlex: 0,
+            nextFlex: 0,
+            showSkipButton: true,
+            curve: Curves.bounceIn,
+            skip: const Text('Skip'),
+            onDone: () => _onIntroEnd(context),
+            onSkip: () => _onIntroEnd(context),
+            next: const Icon(Icons.arrow_forward),
+            done: const Text('Done',
+                style: TextStyle(fontWeight: FontWeight.w600)),
+            dotsDecorator: const DotsDecorator(
+              size: Size(10.0, 10.0),
+              color: Color(0xFFBDBDBD),
+              activeSize: Size(22.0, 10.0),
+              activeColor: Palette.primaryColor,
+              activeShape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(25.0)),
+              ),
+            ),
+          );
+        }
+      },
     );
   }
 }
