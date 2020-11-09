5e795527d8fd505b645f8f85971e32e98d7c19db
mina gerges <minagerges4@gmail.com>
2020-11-09 21:50:02 +0200

    add intro ip

--- front-end/lib/config/constant.dart
+++ front-end/lib/config/constant.dart
@@ -1,5 +1,3 @@
-String googleMapAPI = '';
-
 String baseUrl = "http://35.180.35.89:8090";
 String baseUrlRoutes = "http://35.180.35.89:9292";
-String baseUrlIntroductionScreen = "http://10.0.2.2:5000";
+String baseUrlIntroductionScreen = "http://35.180.35.89:5000";
