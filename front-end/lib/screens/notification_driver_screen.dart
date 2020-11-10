dc980d881324078f8107e4ff0e366c21596e632c
mina.gerges <minagerges4@gmail.com>
2020-11-10 13:17:59 +0200

    new driver notification view

--- front-end/lib/screens/notification_driver_screen.dart
+++ front-end/lib/screens/notification_driver_screen.dart
@@ -3,6 +3,8 @@ import 'package:fleva_icons/fleva_icons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:khedni_maak/context/notification_provider.dart';
+import 'package:khedni_maak/functions/functions.dart';
+import 'package:khedni_maak/widgets/display_flash_bar.dart';
 import 'package:khedni_maak/widgets/error_widget.dart';
 import 'package:provider/provider.dart';
 
@@ -18,6 +20,8 @@ class NotificationDriverScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<AnimatedListState> listKey = GlobalKey<AnimatedListState>();
+
     NotificationProvider notificationProvider =
         Provider.of<NotificationProvider>(context);
 
@@ -25,7 +29,6 @@ class NotificationDriverScreen extends StatelessWidget {
         notificationProvider.getNotificationsRiderRequest;
 
     _removeNotification(String routeId) {
-
       _firebaseMessaging.unsubscribeFromTopic('route-$routeId');
 
       for (int i = 0; i < notificationsRiderRequest.length; i++) {
@@ -41,10 +44,14 @@ class NotificationDriverScreen extends StatelessWidget {
 
     _onAcceptRequest(String routeId) {
       _removeNotification(routeId);
+
+      DisplayFlashBar.displayFlashBar('success', 'Request accepted', context);
     }
 
     _onDeclineRequest(String routeId) {
       _removeNotification(routeId);
+
+      DisplayFlashBar.displayFlashBar('success', 'Request rejected', context);
     }
 
     if (notificationsRiderRequest.length == 0)
@@ -54,73 +61,104 @@ class NotificationDriverScreen extends StatelessWidget {
         imagePath: 'truck.png',
       );
 
-    return ListView.builder(
-      // padding: const EdgeInsets.all(5),
-      itemCount: notificationsRiderRequest.length,
-      itemBuilder: (BuildContext context, int index) {
+    return AnimatedList(
+      key: listKey,
+      initialItemCount: notificationsRiderRequest.length,
+      itemBuilder: (context, index, animation) {
         final route = notificationsRiderRequest[index];
         String from = route['from'];
         String to = route['to'];
         String riderUsername = route['riderUsername'];
         String routeId = route['routeId'];
+        String requestDateTime = route['requestDateTime'];
+        String requestDateTimeStamp =
+            Functions.timeAgoSinceDate(requestDateTime);
 
-        return Container(
-          padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.blue[400], Colors.blue[600]],
+        return SizeTransition(
+          axis: Axis.vertical,
+          sizeFactor: animation,
+          child: SizedBox(
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.blue[50], Colors.blue[50]],
+                ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  RichText(
+                    text: TextSpan(
+                      // text: riderUsername,
+                      style: TextStyle(color: Colors.black),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: riderUsername,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ' request to join you from ',
+                        ),
+                        TextSpan(
+                          text: from,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        TextSpan(
+                          text: ' To ',
+                        ),
+                        TextSpan(
+                          text: to,
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text(
+                    requestDateTimeStamp,
+                    style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 12,
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      RaisedButton(
+                        onPressed: () => _onAcceptRequest(routeId),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        color: Colors.green[600],
+                        textColor: Colors.white,
+                        child: Row(
+                          children: [
+                            Icon(Icons.check),
+                            Text("Accept"),
+                          ],
+                        ),
+                      ),
+                      RaisedButton(
+                        onPressed: () => _onDeclineRequest(routeId),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        color: Colors.red[600],
+                        textColor: Colors.white,
+                        child: Row(
+                          children: [
+                            Icon(FlevaIcons.close),
+                            Text("Reject"),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              SizedBox(height: 5.0),
-              Row(
-                children: [
-                  Text("$riderUsername request to go with you !"),
-                ],
-              ),
-              SizedBox(height: 5.0),
-              Row(
-                children: [
-                  Text("From "),
-                  Text(from),
-                  Text(" To "),
-                  Text(to),
-                ],
-              ),
-              SizedBox(height: 5.0),
-              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-                RaisedButton(
-                  onPressed: () => _onAcceptRequest(routeId),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  color: Colors.green[600],
-                  textColor: Colors.white,
-                  child: Row(
-                    children: [
-                      Icon(Icons.check),
-                      Text("Accept"),
-                    ],
-                  ),
-                ),
-                RaisedButton(
-                  onPressed: () => _onDeclineRequest(routeId),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  color: Colors.red[600],
-                  textColor: Colors.white,
-                  child: Row(
-                    children: [
-                      Icon(FlevaIcons.close),
-                      Text("Reject"),
-                    ],
-                  ),
-                ),
-              ])
-            ],
           ),
         );
         //onTap:onCarTap,
