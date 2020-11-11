f9e7929be5e8a164e8b088661039acc488626c75
mina.gerges <minagerges4@gmail.com>
2020-11-11 17:30:27 +0200

    remove data

--- front-end/lib/config/Secrets.dart
+++ front-end/lib/config/Secrets.dart
@@ -1,4 +1,4 @@
 class Secrets {
-  static const API_KEY = 'AIzaSyCRFrdnwMkAr-rhTvr9Z1CWIS3kYeo8o6Y';
-  static const FCMServer_Token = 'AAAAxflvIrQ:APA91bFnReP7UhMbTSxGqpe1v2cG1FSkrP5lqwN2iyWrZDbXv-CfbPoHtYkUr9mPpU8-aOaaldAIwjXs7WUGh2Yi_1HNMu6S6zAW_We_0ULFq_ql9aYCnWhNiqb_7j8Jb7sD4Ue6amnY';
+  static const API_KEY = '';
+  static const FCMServer_Token = '';
 }
