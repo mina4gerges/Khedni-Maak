93b69d54cb98134ce05d2cd89e30579b6a0f6959
mina.gerges <minagerges4@gmail.com>
2020-11-11 17:28:36 +0200

    fix history screen data

--- front-end/lib/functions/functions.dart
+++ front-end/lib/functions/functions.dart
@@ -83,22 +83,27 @@ class Functions {
   }
 
   //filter routes with status 1 (available)
- static List filterRoutesStatus(List routes,String userFullName,String source) {
+  static List filterRoutesStatus(
+      List routes, String userFullName, String source) {
     //show only driver route
     if (source == 'driver')
       return routes
           .where((route) =>
-      route["status"] == 1 &&
-          route['driverUsername'] == userFullName)
+              route["status"] == 1 && route['driverUsername'] == userFullName)
           .toList();
     //show all routes different then loginUserName
     else
       return routes
           .where((route) =>
-      route["passengers"].indexOf(userFullName) == -1 &&
-          route["status"] == 1 &&
-          route['driverUsername'] != userFullName)
+              route["passengers"].indexOf(userFullName) == -1 &&
+              route["status"] == 1 &&
+              route['driverUsername'] != userFullName)
           .toList();
   }
 
+  static List getHistoryList(List routes, String userFullName) {
+    return routes
+        .where((route) => route["passengers"].indexOf(userFullName) > -1)
+        .toList();
+  }
 }
--- front-end/lib/screens/history_screen.dart
+++ front-end/lib/screens/history_screen.dart
@@ -1,14 +1,79 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:khedni_maak/config/constant.dart';
+import 'package:khedni_maak/functions/functions.dart';
 import 'package:khedni_maak/widgets/error_widget.dart';
+import 'package:http/http.dart' as http;
+import 'package:khedni_maak/widgets/routes_list.dart';
+import 'package:khedni_maak/config/globals.dart' as globals;
 
-class HistoryScreen extends StatelessWidget {
+class HistoryScreen extends StatefulWidget {
+
+  HistoryScreen({
+    Key key,
+    @required this.source,
+  }) : super(key: key);
+
+  final String source;
+
+  @override
+  _HistoryScreenState createState() => _HistoryScreenState();
+}
+
+class _HistoryScreenState extends State<HistoryScreen> {
+
+  Future _routesFuture;
+  bool isLoadingData = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _routesFuture = _getRoutes();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  Future _getRoutes() async {
+    return await http.get('$baseUrlRoutes/routes');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ErrorWidgetDisplay(
-      title: 'HISTORY',
-      subTitle: 'HISTORY',
-      imagePath: 'truck.png',
+    return FutureBuilder(
+      future: _routesFuture,
+      builder: (context, snap) {
+        if (snap.connectionState == ConnectionState.waiting)
+          return const Center(child: CircularProgressIndicator());
+        else if (snap.data == null) {
+          return ErrorWidgetDisplay(
+            title: 'Error',
+            subTitle: 'Error',
+            imagePath: 'error.png',
+          );
+        } else {
+          List routes = json.decode(snap.data.body);
+
+          List filteredRoutes = Functions.getHistoryList(
+              routes, globals.userFullName);
+
+          if (filteredRoutes != null && filteredRoutes.isEmpty)
+            return ErrorWidgetDisplay(
+              title: 'No history',
+              subTitle: 'History are empty',
+              imagePath: 'truck.png',
+            );
+          else
+            return RoutesList(
+              routes: filteredRoutes,
+              source: widget.source,
+            );
+        }
+      },
     );
   }
 }
